c0229c5b7fb323f4b467718d6b051666df05aba3
saransh17 <saranshj98@users.noreply.github.com>
2018-09-29 17:30:10 +0530

    port variable declared

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use('/api', routes);
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? 80 : 8080;
+const port = process.env.PORT;
 const server = app.listen(port, function () {
     console.log('Server listening on port ' + port);
 });
