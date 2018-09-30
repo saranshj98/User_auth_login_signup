4c063b8d1fc9f72eb79885c697cf765f69aaa700
saransh17 <saranshj98@users.noreply.github.com>
2018-09-30 18:04:58 +0530

    waterfall method implemented

--- app/routes/User/post.js
+++ app/routes/User/post.js
@@ -1,5 +1,6 @@
 const mongoose  = require('mongoose')
 const User      = require('../../model/user')
+const async     = require('async')
 
 
 function authUser(req, res) {
@@ -15,86 +16,116 @@ function authUser(req, res) {
         })
     }
 
-    //user try to signup
-    else if(username) {
-        User.findOne({ "email": email }).exec((err, docs) => {
+    if(username) { signUp() } 
+    else { login() }
+
+     //user try to signup
+    function signUp() {
+
+        async.waterfall([
+            findUser,
+            checkUser
+        ], function(err, result) {
             if (err) { 
                 return res.send({
                     error : true,
                     message : "Database Error"
                 })
             }
-            
-            else if (docs) {
+
+            else {
+                return res.send({
+                    error   : false,
+                    message : "Signup done successfully",
+                    userDoc : result
+                })
+            }
+        })
+
+
+        function findUser(callback) {
+            User.findOne({ "email" : email}).exec((err, docs) => {
+                if(err) { return callback(err) }
+
+                else { return callback(null, docs)}
+            })
+        }
+
+
+        function checkUser(userDoc, callback) {
+            if(userDoc) {
                 return res.send({
                     error : true,
                     message : 'User already exist with this email'
                 });
-            } 
+            }
 
             else {
                 let user                           =   new User()
                 user.username                      =   username;
                 user.email                         =   email;
                 user.password                      =   user.generateHash(password);
-
                 user.save(er => {
-                    if (er) { 
-                        return res.send({
-                            error: true,
-                            message: "Database error"
-                        })
-                    }
+                    if (er) { return callback(er) }
 
-                    else {
-                        return res.send({
-                            error : false,
-                            message: "Signup done successfully",
-                            userDoc: user
-                        })
-                    }
-                    
+                    else { return callback(null, user) }
                 })
             }
-        })
+        }
     }
 
     //user try to login
-    else {
-        User.findOne({ "email": email }).exec((err, docs) => {
+    function login() {
+
+        async.waterfall([
+            findUser,
+            checkUser
+        ], function(err, result) {
             if (err) { 
                 return res.send({
-                    error : true,
+                    error   : true,
                     message : "Database Error"
                 })
             }
 
-            else if (docs) {
+            else {
+                return res.send({
+                    error   : false,
+                    message : "Login done successfully",
+                    userDoc : result
+                })
+            }
+        })
 
-                if (docs.validPassword(password)) {
-                    return res.send({
-                        error : false,
-                        message : "Login done successfully",
-                        userDoc: docs
-                    })
-                } 
 
-                else { 
-                    return res.send({
-                        error   : true,
-                        message : "Password is incorrect"
-                    })
-                }
+        function findUser(callback) {
+            User.findOne({ "email": email }).exec((err, docs) => {
+                if(err) { return callback(err) }
 
+                else { return callback(null, docs)}
+            })
+        }
+
+        function checkUser(userDoc, callback) {
+            if(userDoc && userDoc.validPassword(password)) {
+                return callback(null, userDoc)
+            }
+
+            else if(userDoc && !userDoc.validPassword(password)) {
+                return res.send({
+                    error   : true,
+                    message : "Password is incorrect"
+                })
             }
 
             else { 
                 return res.send({
-                    error : true,
+                    error   : true,
                     message : "Enter valid email and password"
                 }) 
-            } 
-        })
+            }
+        }
+
     }
 }
 
